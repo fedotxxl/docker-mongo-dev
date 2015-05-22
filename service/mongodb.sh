0ce6106de43fbf4cf8601e29ee048d5c9d001c06
fbelov <fedor.belov@mail.ru>
2015-05-22 21:53:24 +0300

    log queries

--- service/mongodb.sh
+++ service/mongodb.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
+#http://stackoverflow.com/questions/15204341/mongodb-logging-all-queries
 
 mkdir -p /job/mongo/data
 mkdir -p /job/mongo/log
-mongod --dbpath /job/mongo/data --smallfiles --logpath /job/mongo/log/mongodb.log >> /job/mongo/log/start.log 2>&1
+mongod --profile=1 --slowms=1 --dbpath /job/mongo/data --smallfiles --logpath /job/mongo/log/mongodb.log >> /job/mongo/log/start.log 2>&1
